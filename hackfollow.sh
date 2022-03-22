ddaedf7c3c90a7abad8d073dd2d7dee5de312112
glitchCodes47 <97714837+glitchCodes47@users.noreply.github.com>
2022-03-23 01:29:05 +0700

    Delete

--- hackfollow.sh
+++ hackfollow.sh
@@ -1,96 +1,5 @@
-#!/bin/bash
-# insfollow v2.0
-# recoded by: Termux Professor
-# github.com/termuxprofessor/insfollow
-# If you use any part from this code, give me the credits, please, read the License
-
-clear
-string4=$(openssl rand -hex 32 | cut -c 1-4)
-string8=$(openssl rand -hex 32  | cut -c 1-8)
-string12=$(openssl rand -hex 32 | cut -c 1-12)
-string16=$(openssl rand -hex 32 | cut -c 1-16)
-device="android-$string16"
-uuid=$(openssl rand -hex 32 | cut -c 1-32)
-phone="$string8-$string4-$string4-$string4-$string12"
-guid="$string8-$string4-$string4-$string4-$string12"
-header='Connection: "close", "Accept": "*/*", "Content-type": "application/x-www-form-urlencoded; charset=UTF-8", "Cookie2": "$Version=1" "Accept-Language": "en-US", "User-Agent": "Instagram 10.26.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)"'
-var=$(curl -i -s -H "$header" https://i.instagram.com/api/v1/si/fetch_headers/?challenge_type=signup&guid=$uuid > /dev/null)
-var2=$(echo $var | grep -o 'csrftoken=.*' | cut -d ';' -f1 | cut -d '=' -f2)
-ig_sig="4f8732eb9ba7d1c8e8897a75d6474d4eb3f5279137431b2aafb71fafe2abe178"
-
-
-
-banner() {
-echo ""
-
+#!
 echo -e 
-echo -e "\e[1;91m                :!777~.                 "
-echo -e "\e[1;91m               ~YJ!~~7!^                "
-echo -e "\e[1;91m               J7     J?^               "
-echo -e "\e[1;91m               J?^~!!!BJJ               "
-echo -e "\e[1;91m               77??!!!^:J.              "
-echo -e "\e[1;91m               !!.?JJ7  ?               "
-echo -e "\e[1;91m               7! ^~:.  J.              "
-echo -e "\e[1;91m               ^?       7J              "
-echo -e "\e[1;91m               .Y       .P. ~7:         "
-echo -e "\e[1;91m                5..:^^.  P~5Y!J7^       "
-echo -e "\e[1;91m          ..:^^^P.^~^^^  JPGP5Y5B~      "
-echo -e "\e[1;91m        .7J5PP?JB::^^~!: 7?^7J!~GBJ.    "
-echo -e "\e[1;91m   .~??J?::^:.  5::~!!:  ~^     :J75~   "
-echo -e "\e[1;91m  ~P5?::!       J.       ~!      Y^:5J  "
-echo -e "\e[1;91m  ?^   :?       J:       ~!      YJ :&~ "
-echo -e "\e[1;91m  J.   .?       J^       ~~      !! YY. "
-echo -e "\e[1;91m .P.    ?       ?^       ~!      :GP!   "
-echo -e "\e[1;91m  P^   .P.      J:       !J      .B~    "
-echo -e "\e[1;91m :G^   :Y.      7.       :^      :P.    "
-echo -e "\e[1;91m .Y.    ^:               :.      :P     "
-echo -e "\e[1;91m  :!~^:.7.        :.     ^P:    .7~     "
-echo -e "\e[1;91m    :^^!!.       ^?       J:.:^7!.      "
-echo -e "\e[1;91m        ^?!^.......      .!?7!^:        "
-echo -e "\e[1;91m          ~?J?!!7??7~::^7Y!             "
-echo -e "\e[1;91m                  .^~7?7!.              "
-echo -e "\e[1;91m                                        "
-
-echo -e "\e[1;91m ═╦═\e[1;92m┌─┐ ┌┌──┐\e[1;91m╔══╗\e[1;92m┌──┐┬   ┬   ┌──┐┬ ┬ ┬"
-
-echo -e "\e[1;91m  ║ \e[1;92m│ │ ││   \e[1;91m║   \e[1;92m│  ││   │   │  ││ │ │"
-
-echo -e "\e[1;91m  ║ \e[1;92m│ │ │└──┐\e[1;91m╠═╣ \e[1;92m│  ││   │   │  ││ │ │"
-
-echo -e "\e[1;91m  ║ \e[1;92m┘ └─┘└──┘\e[1;91m║   \e[1;92m└──┘┴──┘┴──┘└──┘└─┴─┘"
-
-echo -e "\e[1;91m ═╩═\e[1;92m         \e[1;91m╩   by - \e[1;92mSuntik By Glitch Notes"
-
-}
-
-
-login_user() {
-
-
-if [[ $user == "" ]]; then
-printf "\n"
-printf "  \e[1;31m[\e[0m\e[1;77m*\e[0m\e[1;31m]\e[0m\e[1;93m Login\e[0m\n"
-read -p $'  \e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Username: \e[0m' user
-fi
-
-if [[ -e cookie.$user ]]; then
-
-printf "  \e[1;31m[\e[0m\e[1;77m*\e[0m\e[1;31m]\e[0m\e[1;93m Cookies found for user\e[0m\e[1;77m %s\e[0m\n" $user
-
-default_use_cookie="Y"
-
-read -p $'  \e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;93m Use it?\e[0m\e[1;77m [Y/n]\e[0m ' use_cookie
-
-use_cookie="${use_cookie:-${default_use_cookie}}"
-
-if [[ $use_cookie == *'Y'* || $use_cookie == *'y'* ]]; then
-printf "  \e[1;31m[\e[0m\e[1;77m*\e[0m\e[1;31m]\e[0m\e[1;93m Using saved credentials\e[0m\n"
-else
-rm -rf cookie.$user
-login_user
-fi
-
-
 else
 
 read -s -p $'  \e[1;31m[\e[0m\e[1;77m*\e[0m\e[1;31m]\e[0m\e[1;93m Password: \e[0m' pass
@@ -314,4 +223,4 @@ fi
 
 
 banner
-menu
+
